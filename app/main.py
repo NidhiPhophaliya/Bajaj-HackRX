8f85a8c4a046cb6fafc7e6b75664eca0aa8ca02c
Nidhi-Phophaliya <nidhiphophaliya945@gmail.com>
2025-07-31 22:00:31 +0530

    Move semantic engine initialization inside the request handler for better encapsulation

--- app/main.py
+++ app/main.py
@@ -10,11 +10,11 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 app = FastAPI()
 
-# Load semantic engine
-search_engine = SemanticSearch("app/data/chunks.csv")
+
 
 @app.post("/hackrx/run", response_model=QueryResponse)
 def run_handler(request: Request, payload: QueryRequest, authorization: str = Header(None)):
+    search_engine = SemanticSearch("app/data/chunks.csv")
     if not authorization or not authorization.startswith("Bearer ") or authorization.split()[1] != API_KEY:
         raise HTTPException(status_code=401, detail="Unauthorized")
 
